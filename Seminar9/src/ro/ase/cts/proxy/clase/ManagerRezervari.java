3f4b7c25a276cbddf51904376b832cdb5cb131cb
Stan Adrian <stanadrian18@stud.ase.ro>
2021-04-21 08:33:16 +0300

    Seminar 9, Proxy

---
+++ Seminar9/src/ro/ase/cts/proxy/clase/ManagerRezervari.java
@@ -0,0 +1,17 @@
+package ro.ase.cts.proxy.clase;
+
+public class ManagerRezervari implements Rezervabil{
+
+
+    private String numeLocal;
+
+
+    public ManagerRezervari(String numeLocal) {
+        this.numeLocal = numeLocal;
+    }
+
+    @Override
+    public void rezerva(int nrPersoane) {
+        System.out.println("A fost realizata o rezervare de " + nrPersoane + " peroane la localul " + this.numeLocal);
+    }
+}
